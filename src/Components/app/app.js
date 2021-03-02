2205e741f06c63269fc149b368e1a966ba95142e
Konstantin <konstantin-eka@ya.ru>
2021-03-02 23:01:20 +0300

    structure first

--- src/Components/app/app.js
+++ src/Components/app/app.js
@@ -1,7 +1,31 @@
 import React from "react";
+import AppHeader from "../app-header/app-header";
+import SearchPanel from "../search-panel/search-panel";
+import PostStatusFilter from "../post-status-filter/post-status-filter";
+import PostList from "../post-list/post-list";
+import PostAddForm from "../post-add-form/post-add-form";
+import '../../index.css';
+
+import './app.css';
+import '../search-panel/search-panel.css';
+import '../app-header/app-header.css';
+import '../post-add-form/post-add-form.css';
+import '../post-list/post-list.css';
+import '../post-list-item/post-list-item.css';
+import '../post-status-filter/post-status-filter.css';
 
 const App = () => {
-    return (<h1>Hello</h1>)
+    return (
+        <div className="app">
+            <AppHeader/>
+            <div className="search-panel d-flex">
+                <SearchPanel/>
+                <PostStatusFilter/>
+            </div>
+            <PostList/>
+            <PostAddForm/>
+        </div>
+    )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from '../src/Components/app/app';
-
+import AppHeader from "./Components/app-header/app-header";
 
 
 
