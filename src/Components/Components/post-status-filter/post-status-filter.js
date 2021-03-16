690c613b958a43754b67cb7af69c9629e43a60f8
webdevek <webdevek@webdevek.ru>
2021-03-16 21:57:31 +0300

    commit finish app

---
+++ src/Components/Components/post-status-filter/post-status-filter.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+// import { Button } from 'reactstrap'; // для импорта кнопок
+
+// import 'post-status-filter.css';
+
+export default class PostStatusFilter extends Component {
+    constructor(props) {
+        super(props);
+        this.buttons = [
+            {name: 'all', label: 'Все'},
+            {name: 'like', label: 'Понравилось'},
+
+        ]
+    }
+    render() {
+        const buttons = this.buttons.map(({name, label}) => {
+
+            const {filter, onFilterSelect} = this.props;
+            const active = filter === name;
+            const clazz = active ? 'btn-info' : 'btn-outline-secondary'
+            return (
+               <button
+                   key={name}
+                   type='button'
+                   className={`btn ${clazz}`}
+                   onClick={() => onFilterSelect(name)}>{label}</button>
+           )
+        });
+        return (
+            <div className="btn-group">
+                {buttons}
+            </div>
+        )
+    }
+}
+
+
+
+// const PostStatusFilter = () => {
+//     return (
+//         <div className="btn-group">
+//             {/*<Button outline color='info'>Все</Button> */}
+//             <button type='button' className="btn btn-info">Все</button>
+//             <button type='button' className="btn btn-outline-secondary">Понравилось</button>
+//         </div>
+//     )
+// }
+// export default PostStatusFilter;
